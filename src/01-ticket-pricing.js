4e40c9f9c89286f68d2bdc2fb45a894ad4b4bc57
Pratik Bobde <pratikbobde614@gmail.com>
2026-06-01 19:52:19 +0530

    complete ticket pricing solution

--- src/01-ticket-pricing.js
+++ src/01-ticket-pricing.js
@@ -24,4 +24,31 @@
  */
 export function getTicketPrice(age, isWeekend) {
   // Your code here
+
+  if (age < 0 || typeof age !== "number") {
+    return - 1;
+  }
+  let price;
+
+  if (age >= 0 && age <= 12) {
+    price = 8;
+  }
+
+  else if (age >= 13 && age <= 17) {
+    price = 12;
+  }
+
+  else if (age >= 18 && age <= 59) {
+    price = 15;
+  }
+
+  else {
+    price = 10;
+  }
+
+  if (isWeekend) {
+    price += 3;
+  }
+
+  return price;
 }
