5b8e89a2b089a9d63456259098a2ff7839dbc02e
Pratik Bobde <pratikbobde614@gmail.com>
2026-06-03 15:07:18 +0530

    completetill coffee  shop question

--- src/01-ticket-pricing.js
+++ src/01-ticket-pricing.js
@@ -26,23 +26,17 @@ export function getTicketPrice(age, isWeekend) {
   // Your code here
 
   if (age < 0 || typeof age !== "number") {
-    return - 1;
+    return -1;
   }
   let price;
 
   if (age >= 0 && age <= 12) {
     price = 8;
-  }
-
-  else if (age >= 13 && age <= 17) {
+  } else if (age >= 13 && age <= 17) {
     price = 12;
-  }
-
-  else if (age >= 18 && age <= 59) {
+  } else if (age >= 18 && age <= 59) {
     price = 15;
-  }
-
-  else {
+  } else {
     price = 10;
   }
 
